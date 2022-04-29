2bd02032f7f1250a4a921f8f811c2082294a278c
Joe <79620069+JoeIsGod@users.noreply.github.com>
2022-04-28 19:32:38 -0500

    Update execute.lua

--- execute.lua
+++ execute.lua
@@ -1,4 +1,10 @@
 rawset(_G, "CherryExecute", function(code)
-  local bCode = rawget(_G, "CherryCompile")(code)
-  rawget(_G, "CherryInterpreter")(bcode)
+    print(code)
+    local success, err = pcall(fuinction() 
+        local bCode = rawget(_G, "CherryCompile")(code)
+        rawget(_G, "CherryInterpreter")(bcode)
+    end)
+    if success == false then
+        error("[EXECUTE ERROR]: "..tostring(err))
+    end
 end)
